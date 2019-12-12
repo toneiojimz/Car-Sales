0e1c0f44ea91030fbbca0fbbce5ae57ea4415b00
toneiojimz <ajimeneza77@gmail.com>
2019-12-12 02:01:18 -0800

    close to MVP needs onClick to add feature

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+import { addItem, removeItem} from '../Actions/FeatureActions';
 
 const AddedFeature = props => {
+  const [newFeat, setNewFeat] = useState();
+
+  const handleChanges = e => {
+    setNewFeat(e.target.value);
+  };
+
   return (
-    <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
-    </li>
+    <div>
+      {!props.editting ? (
+        <h1>
+          {props.feature.name} 
+          <i onClick={props.addItem}/>
+        </h1>
+      ) : (
+      <li>
+        {/* Add an onClick to run a function to remove a feature */}
+        <button className="button" onClick={() => props.removeItem(newFeat)} >Features Updated</button>
+        {props.feature.name}
+      </li>
+      )}
+    </div>
   );
 };
 
